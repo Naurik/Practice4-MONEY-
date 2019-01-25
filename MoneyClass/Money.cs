bd00d18a6254d82389d8488ffcc783f769429eb7
Nauryzbai <nauzik@mail.ru>
2019-01-25 14:33:14 +0600

    Класс Money

---
+++ MoneyClass/Money.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMoney
+{
+    public class Money
+    {
+        private string _denominationOfBills;
+        private int _numberOfBills;
+
+        public Money(string denominationOfBills, int numberOfBills)
+        {
+            _denominationOfBills = denominationOfBills;
+            _numberOfBills = numberOfBills;
+        }
+        public Money(int numberOfBills)
+        {
+            GetNumber();
+            if (numberOfBills > _numberOfBills)
+            {
+                Console.WriteLine("Денег не хватает!!!");
+            }
+            else
+                Console.WriteLine("Денег хватает!!!");
+        }
+
+        #region Methods
+        public string GetDenomination() { return _denominationOfBills; }
+        public void SetDenomination(string denominationB)
+        {
+            _denominationOfBills = denominationB;
+        }
+        public int GetNumber() { return _numberOfBills; }
+        public void SetNumber(int numberB)
+        {
+            _numberOfBills = numberB;
+        }
+
+        public bool MoneyN(int numberOfBills)
+        {
+            if (numberOfBills > _numberOfBills)
+            {
+                Console.WriteLine("Денег не хватает!!!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Денег хватает!!!");
+                return true;
+            }
+        }
+        public void AmountThings(int things)
+        {
+            int yes = _numberOfBills / things;
+            Console.WriteLine("Кол-во вещей которые можно купить = " + yes);
+        }
+        #endregion
+
+    }
+}
---
+++ MoneyClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money("Euro", 100);
+            money.SetDenomination("Dollar");
+            money.SetNumber(25);
+
+            Console.WriteLine("Кол-во купюр = " + money.GetNumber());
+            Console.WriteLine("Номинал купюры = " + money.GetDenomination());
+            money.MoneyN(20);
+            money.AmountThings(5);
+
+            Console.ReadLine();
+        }
+    }
+}
